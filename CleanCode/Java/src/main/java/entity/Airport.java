5448a33f1ce5685e8e59e9dc1ad05fdf07db9134
Albina Bondarchik <erionolsen@gmail.com>
2020-12-05 23:18:02 +0300

    Change .gitignore configuration

--- CleanCode/Java/src/main/java/entity/Airport.java
+++ CleanCode/Java/src/main/java/entity/Airport.java
@@ -17,7 +17,7 @@ public class Airport {
     }
 
     public List<? extends Plane> getPlanes() {
-        List<? extends Plane> planesCopy = planes.;
+        List<? extends Plane> planesCopy = planes;
         return planes;
     }
 
